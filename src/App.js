802422d8c5d9e2435faa7e25951c76fb5f6cd283
ravi <rafael.vitor@jusbrasil.com.br>
2018-10-30 22:54:35 -0300

    Improve code style

--- src/App.js
+++ src/App.js
@@ -1,61 +1,56 @@
-import React, { Component } from 'react';
-import { Chart } from './components/chart'
-import { getData } from './components/get'
+import React, { Component } from "react";
+import { Chart } from "./components/chart";
+import { getData } from "./components/get";
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            data: [],
-            time: []
-        }
-        this.getDataRequest = this.getDataRequest.bind(this)
-    }
-
-    componentDidMount() {
-        setInterval(this.getDataRequest, 50000);
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      time: []
     };
+  }
 
+  componentDidMount() {
+    setInterval(this.getDataRequest, 5000);
+  }
 
-    async getDataRequest() {
+  getDataRequest = async () => {
+    let databtc = await getData;
+    console.log("databtc");
+    console.log(databtc);
+    //console.log(databtc.BRL.buy);
 
-        let databtc = await getData;
-        console.log('databtc')
-        console.log(databtc);
-        //console.log(databtc.BRL.buy);
+    let info = databtc.data.BRL.buy;
+    console.log("info");
+    console.log(info);
 
-        let info = databtc.data.BRL.buy
-        console.log('info')
-        console.log(info)
+    let time = new Date();
+    let currentTime =
+      time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds();
 
-        let time = new Date();
-        let currentTime = time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds();
+    let joinTime = this.state.time.concat(currentTime);
 
-        let joinTime = this.state.time.concat(currentTime)
+    let join = this.state.data.concat(info);
 
-        let join = this.state.data.concat(info)
+    this.setState({
+      data: join,
+      time: joinTime,
+      lastData: info
+    });
 
-        this.setState({
-            data: join,
-            time: joinTime,
-            lastData: info,
-        })
+    console.log("state");
+    console.log(this.state);
+  }
 
-
-
-        console.log('state')
-        console.log(this.state)
-    };
-
-
-    render() {
-        return (
-            <div className="App">
-                <Chart data={this.state.data} time={this.state.time} />
-                <br />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <Chart data={this.state.data} time={this.state.time} />
+        <br />
+      </div>
+    );
+  }
 }
 
 export default App;
